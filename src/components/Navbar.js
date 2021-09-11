e8b3efe1ff0c2f57cb152c407157bbfc79117120
gouri bhise <gouri.a.bhise@gmail.com>
2021-09-11 12:13:20 +0530

    updated navbar styling with styled components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom';
 import logo from '../logo.svg'
+import styled from 'styled-components'
+import {ButtonContainer} from './Button'
 export default class Navbar extends Component{
   render(){
     return(
-    <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+    <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
         {/*
 
           https://www.iconfinder.com/icons/1243689/call_phone_icon
@@ -20,12 +22,23 @@ export default class Navbar extends Component{
           </li>
           </ul>
           <Link to="/cart" className="ml-auto">
-          <button>
+          <ButtonContainer>
+          <span className="mx-2">
             <i className="fas fa-cart-plus"/>
+            </span>
             My Cart
-          </button>
+          </ButtonContainer>
           </Link>
-    </nav>
+    </NavWrapper>
     )
   }
 }
+
+const NavWrapper=styled.nav`
+background:var(--mainBlue);
+.nav-link{
+  color:var(--mainWhite) !important;
+  font-size:1.3rem;
+  text-transform:capitalize !important;
+}
+`;
