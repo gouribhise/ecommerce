073e7180d6a83b32efcd918e8e7d30303af2ca29
gouri bhise <gouri.a.bhise@gmail.com>
2021-09-12 11:21:08 +0530

    added products details and cart button

--- src/components/Product.js
+++ src/components/Product.js
@@ -10,11 +10,21 @@ export default class Product extends Component{
        <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
          <div className="card">
             <div className="img-container p-5"
-            onClick={console.log("you clicked image")}>
+            onClick={()=>console.log("you clicked image")}>
 
               <Link to="/details">
               <img src={img} alt="image of product" className="card-img-top"/>
               </Link>
+              <button className="cart-btn" disabled={inCart?true:false}
+              onClick={()=>{console.log("added to cart")}}>
+              {inCart?(<p className="text-capitalize mb-0" disabled>In Cart</p>):(<i className="fas fa-cart-plus"/>)}
+              </button>
+            </div>
+            <div className="card-footer d-flex justify-content-between">
+                <p className="align-self-center mb-0">
+                  {title}
+                </p>
+                <h5 className="text-blue font-italic mb-0"><span className="mr-1">$</span> {price}</h5>
             </div>
          </div>
        </ProductWrapper>
@@ -23,5 +33,23 @@ export default class Product extends Component{
 }
 
 const ProductWrapper=styled.div`
+ .card{
+   border-color:transparent;
+   transition:all 1s linear;
 
+ }
+ .card-footer{
+   background:transparent;
+   border-top:transparent;
+   transition:all 1s linear;
+ }
+ &:hover{
+   .card{
+     border:0.04rem solid  rgba(0,0,0,0.2);
+     box-shadow:2px 2px 5px 0px rgba(0,0,0,0.2);
+   }
+   .card-footer{
+     background:rgba(247,247,247)
+   }
+ }
 `;
