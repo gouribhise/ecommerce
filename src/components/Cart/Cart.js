7b125049270b2cee0078bb3ec8a529187bad27d7
gouri bhise <gouri.a.bhise@gmail.com>
2021-09-13 09:44:59 +0530

    added to cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,31 +1,32 @@
-import React, {Component} from 'react'
-import Title from '../Title';
-import CartColumns from './CartColumns';
-import EmptyCart from './EmptyCart';
-import {ProductConsumer} from '../../context'
-export default class Cart extends Component{
-  render(){
-    return(
+import React, { Component } from "react";
+import Title from "../Title";
+import CartColumns from "./CartColumns";
+import CartList from "./CartList";
+import CartTotals from "./CartTotals";
+import { ProductConsumer } from "../../context";
+import EmptyCart from "./EmptyCart";
+export default class Store extends Component {
+  render() {
+    return (
       <section>
-      <ProductConsumer>
-      {value=>{
-        const{cart}=value;
-        if(cart.length>0){
-          return(
-            <>
-            <Title name="Your" title="Cart"/>
-
-            <CartColumns/>
-            </>
-          )
-        }
-        else{
-          return(  <EmptyCart/>)
-        }
-      }}
-    </ProductConsumer>
-
+        <ProductConsumer>
+          {value => {
+            const { cart } = value;
+            if (cart.length > 0) {
+              return (
+                <React.Fragment>
+                  <Title name="your" title="cart" />
+                  <CartColumns />
+                  <CartList value={value} />
+                  <CartTotals value={value} history={this.props.history} />
+                </React.Fragment>
+              );
+            } else {
+              return <EmptyCart />;
+            }
+          }}
+        </ProductConsumer>
       </section>
-    )
+    );
   }
 }
