d9f2e693ef497b0f5a473b40ff1617719376f9d6
gouri bhise <gouri.a.bhise@gmail.com>
2021-09-13 09:56:50 +0530

    remove single item from cart

--- src/context.js
+++ src/context.js
@@ -75,7 +75,22 @@ decrement=(id)=>{
   console.log("decrement")
 }
 removeItem=(id)=>{
-  console.log("remove item")
+   let tempProducts=[...this.state.products];
+   let tempCart=[...this.state.cart];
+   tempCart=tempCart.filter(item=>item.id!==id);
+   const index=tempProducts.indexOf(this.getItem(id));
+   let removedProduct=tempProducts[index];
+   removedProduct.inCart=false;
+   removedProduct.count=0;
+   removedProduct.total=0;
+   this.setState(()=>{
+     return{
+       cart:[...tempCart],
+       products:[...tempProducts]
+     }
+   },()=>{
+     this.addTotals();
+   })
 }
 clearCart=()=>{
   this.setState(()=>{
