0c0ae5da273ac6e3e74f201458cda4a4ef1d83e8
gouri bhise <gouri.a.bhise@gmail.com>
2021-09-14 11:20:10 +0530

    rm btn

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import PayPalButton from "./PayPalButton";
 import { Link } from "react-router-dom";
 export default class CartTotals extends Component {
   render() {
